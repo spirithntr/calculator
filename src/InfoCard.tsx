c6795eba8c4314bff4cdd5cf8b003860036bcb99
Ivan Pastukhov <ivan_pastukhov@epam.com>
2020-02-12 11:28:04 +0400

    feat: add stylings to taxes

--- src/InfoCard.tsx
+++ src/InfoCard.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Card, ListGroup, Spinner } from 'react-bootstrap';
+import React, { Component, Fragment } from 'react';
+import { Badge, Card, ListGroup, Spinner } from 'react-bootstrap';
 import { DealerInfo } from './mock/dealer';
 import StarRatings from 'react-star-ratings';
 
@@ -13,6 +13,13 @@ type Props = {
 
 export default class InfoCard extends Component<Props> {
   render(): React.ReactNode {
+    const taxes = this.props.taxes.map((tax, index) => (
+      <Fragment key={index}>
+        <Badge pill variant="primary">
+          {tax}
+        </Badge>{' '}
+      </Fragment>
+    ));
     return (
       <Card className="m-2">
         <Card.Body>
@@ -21,7 +28,7 @@ export default class InfoCard extends Component<Props> {
               <ListGroup.Item>MSRP: {this.props.dealer.msrp}</ListGroup.Item>
               <ListGroup.Item>vehicle: {this.props.dealer.vehicleName}</ListGroup.Item>
               <ListGroup.Item>monthly pay: {this.props.monthlyPay.toFixed(2)}</ListGroup.Item>
-              <ListGroup.Item>taxes: {this.props.taxes.join(', ')}</ListGroup.Item>
+              <ListGroup.Item>taxes: {taxes}</ListGroup.Item>
               <ListGroup.Item>dealer: {this.props.dealer.name}</ListGroup.Item>
               <ListGroup.Item>phone number: {this.props.dealer.phone}</ListGroup.Item>
               <ListGroup.Item>
@@ -29,7 +36,7 @@ export default class InfoCard extends Component<Props> {
                 <StarRatings
                   className="m-2"
                   rating={this.props.dealer.rating}
-                  starRatedColor="blue"
+                  starRatedColor="#007bff"
                   starDimension="1.5em"
                   numberOfStars={5}
                 />
